0372b1af77e75255a10489779fbb67766778ec7c
Betsegaw Tadele <betsegaw.ta@gmail.com>
2017-09-23 01:54:42 -0700

    Build is the actual script that gets run for building

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('setup', shell.task([
 ]))
 
 gulp.task('build', shell.task([
-  'docker run --rm -it --mount type=bind,source="%cd%/src",target=/app --mount type=bind,source="%cd%/scripts",target=/scripts --entrypoint /bin/bash jekyllbuilder:1.0'
+  'docker run --rm -it --mount type=bind,source="%cd%/src",target=/app --mount type=bind,source="%cd%/scripts",target=/scripts --entrypoint /scripts/build.sh jekyllbuilder:1.0'
 ]))
 
 gulp.task('manual', shell.task([
