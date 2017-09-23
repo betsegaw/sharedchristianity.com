dccfc28e90a161a369a33f37f26871a2ea96b3e5
Betsegaw Tadele <betsegaw.ta@gmail.com>
2017-09-23 02:21:44 -0700

    Added comments to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
 var gulp = require('gulp');
 const shell = require('gulp-shell')
 
+//
+// Creates and prepares the docker image on which this will run
+//
 gulp.task('setup', shell.task([
   'docker build . -t jekyllbuilder:1.0'
 ]))
 
+//
+// Builds the /app/sharedchristinity.com site
+//
 gulp.task('build', shell.task([
   'docker run --rm -it --mount type=bind,source="%cd%/src",target=/app --mount type=bind,source="%cd%/scripts",target=/scripts --entrypoint /scripts/build.sh jekyllbuilder:1.0'
 ]))
 
+//
+// Launches a new instance of the container connected to the terminal.
+//
 gulp.task('manual', shell.task([
   'docker run --rm -it --mount type=bind,source="%cd%/src",target=/app --mount type=bind,source="%cd%/scripts",target=/scripts --entrypoint /bin/bash jekyllbuilder:1.0'
 ]))
 
+//
+// Deploys to zeit.co
+//
 gulp.task('deploy', shell.task([
   'now src/sharedchristianity.com/_site/.'
 ]))
